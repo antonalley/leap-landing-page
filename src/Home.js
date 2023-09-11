5540d72668f85421d9f74f077fcf3cd1df493de9
Anton Alley <anton.alley@gmail.com>
2023-09-11 16:03:48 -0600

    Create Account for Beta

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,40 @@
-import { addToWaitlist } from "./functions/api"
+import { useRef, useState } from "react";
+import { addToWaitlist, createAccount } from "./functions/api"
+import { Dialog, DialogTitle, Grid, ListItem, Modal, Paper, TextField, Typography } from "@mui/material";
+import { Button } from "@mui/base";
+import { Box, Stack } from "@mui/system";
 
 
 export default function Home(){
+    const companyName = useRef(null);
+    const email = useRef(null);
+    const password = useRef(null);
+    const [joinedWaitlist, setjoinedWaitlist] = useState(null);
+    const [betaOpen, setBetaOpen] = useState(false);
+    const [createdAccount, setCreatedAccount] = useState(false);
+    const [accountCreateError, setAccountCreateError] = useState(false);
 
     async function joinWaitlist(e){
         e.preventDefault();
-        await addToWaitlist(e.refs.email.value, e.refs.companyName.value);
+        await addToWaitlist(email.current.value, companyName.current.value);
+        setjoinedWaitlist(companyName.current.value);
     }
+
+    function betaClose(){
+        setBetaOpen(false);
+    }
+
+    async function createAdminAccount(e){
+        e.preventDefault();
+        let success = await createAccount(email.current.value, password.current.value, companyName.current.value);
+        if (success){
+            setCreatedAccount(true)
+            setBetaOpen(false);
+        } else{
+            setAccountCreateError(true);
+        }
+    }
+
     return (
         <div id='home'>
             <section className="description">
@@ -19,16 +47,62 @@ export default function Home(){
                 <div>
                     <h2>Get Beta Access</h2>
                     <p>Gain access to our beta version for just $5.</p>
-                    <button className="pay-button">$5 Pay Now</button>
+                    <button className="pay-button" onClick={()=>setBetaOpen(true)}>Join</button>
                 </div>
+                <Dialog
+                    open={createdAccount}
+                    onClose={()=>setCreatedAccount(false)}
+                    >
+                        <DialogTitle>Successfully Created an account</DialogTitle>
+                    </Dialog>
+                <Dialog
+                    open={betaOpen}
+                    onClose={betaClose}
+                    >
+                        <DialogTitle>Create an admin account for your company</DialogTitle>
+                    <Paper elevation={3}>
+                        <Stack spacing={2}>
+                            <form onSubmit={createAdminAccount}>
+                            <ListItem>
+                            <TextField label="Company Name" type="text" fullWidth inputRef={companyName}></TextField>
+                            </ListItem>
+                            <ListItem>
+                            <TextField label="Email" type="email" fullWidth inputRef={email} ></TextField>
+                            </ListItem>
+                            <ListItem>
+                            <TextField label="Password" type="password" fullWidth inputRef={password}></TextField>
+                            </ListItem>
+                            <ListItem>
+                                <Button type="submit" variant="contained" color="primary">Create Account</Button>
+                                <Button variant="outlined" color="error" onClick={()=>setBetaOpen(false)}>Cancel</Button>
+                            </ListItem>
+                            <ListItem>
+                                {accountCreateError && <div style={{color:"red"}}>Failed to create account. Try again.</div>}
+                            </ListItem>
+                            </form>
+                            
+                        </Stack>
+                    </Paper>
+
+                </Dialog>
+                { !joinedWaitlist ?
                 <div>
                     <h2>Join the Waitlist</h2>
+                    <p>Get updates on app progress,<br/> Get notified when stable version is launched,<br/> Discount first month of use</p>
                     <form onSubmit={joinWaitlist}>
-                    <input ref="companyName" type="text" placeholder="Enter your Company name" />
-                    <input ref="email" type="email" placeholder="Enter your email" />
+                    <input ref={companyName} type="text" placeholder="Enter your Company name" />
+                    <input ref={email} type="email" placeholder="Enter your email" />
                     <button type="submit">Join</button>
                     </form>
                 </div>
+                    :
+                <div>
+                    <h2>Welcome {joinedWaitlist && joinedWaitlist}!</h2>
+                    <p>Thank you for joining our waitlist!</p>
+                    <p>You will receive updates from us soon</p>
+                </div>
+                }
+        
                 
             </section>
         </div>
--- src/functions/api.js
+++ src/functions/api.js
@@ -1,5 +1,6 @@
 import { addDoc, collection, doc, setDoc } from "@firebase/firestore";
 import { db } from "./fb_init";
+import { createUserWithEmailAndPassword, getAuth } from "@firebase/auth";
 
 
 export async function addToWaitlist(email, companyName){
@@ -9,4 +10,29 @@ export async function addToWaitlist(email, companyName){
         companyName: companyName
     })
     return true;
+}
+
+export async function createAccount(email, password, companyName){
+    try{
+        console.log('Creating User', email, password, companyName)
+        let auth = getAuth();
+        let result = await createUserWithEmailAndPassword(auth, email, password);
+        if (result && result.user){
+            console.log('Adding user to database')
+            let uid = result.user.uid;
+
+            await addDoc(collection(db, "users"), {
+                account_type: 'admin',
+                status_codes: ["mvp_1"],
+                user_id: uid,
+                company_name: companyName
+            })
+        } else{
+            return false;
+        }
+        return true;
+    } catch {
+        return false;
+    }
+    
 }
